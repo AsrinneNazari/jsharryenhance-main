89237c71cd6867d449bebb80cfa8f0566e5698ee
Asrinne Nazari <asse_89@hotmail.com>
2024-01-12 14:32:48 +0100

    💥 shortened the function and created a new loop!

--- a.js
+++ a.js
@@ -21,10 +21,9 @@ class Car {
   }
 }
 
-
 const cars = [];
 // Generate 20 cars
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 10; i += 1) {
   const name = faker.vehicle.vehicle();
   const type = faker.vehicle.type();
   const color = faker.vehicle.color();
@@ -36,95 +35,18 @@ for (let i = 0; i < 10; i++) {
 }
 
 function createHtmlForCars() {
-  const c = document.getElementsByClassName('productgallery')[0];
-  const x = `<div class="productcard">
-    <img src="${cars[0].img}">
+  for (let i = 0; i < cars.length; i += 1) {
+    const productGallery = document.getElementsByClassName('productgallery')[0];
+    const productCard = `<div class="productcard">
+    
+    <img src="${cars[i].img}">
     <div class="information">
-        <div class="name">${cars[0].name}</div>
-        <div class="description">${cars[0].year}</div>
-        <a class="ctabutton" href="#">${cars[0].price} kr</a>
+        <div class="name">${cars[i].name}</div>
+        <div class="description">${cars[i].year}</div>
+        <a class="ctabutton" href="#">${cars[i].price} kr</a>
     </div>`;
-  c.innerHTML += x;
-  const x2 = `<div class="productcard">
-    <img src="${cars[1].img}">
-    <div class="information">
-        <div class="name">${cars[1].name}</div>
-        <div class="description">${cars[1].year}</div>
-        <a class="ctabutton" href="#">${cars[1].price} kr</a>
-    </div>`;
-  c.innerHTML += x2;
-
-  const x3 = `<div class="productcard">
-    <img src="${cars[2].img}">
-    <div class="information">
-        <div class="name">${cars[2].name}</div>
-        <div class="description">${cars[2].year}</div>
-        <a class="ctabutton" href="#">${cars[2].price} kr</a>
-    </div>`;
-  c.innerHTML += x3;
-
-  const x4 = `<div class="productcard">
-    <img src="${cars[3].img}">
-    <div class="information">
-        <div class="name">${cars[3].name}</div>
-        <div class="description">${cars[3].year}</div>
-        <a class="ctabutton" href="#">${cars[3].price} kr</a>
-    </div>`;
-  c.innerHTML += x4;
-
-  const x5 = `<div class="productcard">
-    <img src="${cars[4].img}">
-    <div class="information">
-        <div class="name">${cars[4].name}</div>
-        <div class="description">${cars[4].year}</div>
-        <a class="ctabutton" href="#">${cars[4].price} kr</a>
-    </div>`;
-  c.innerHTML += x5;
-
-  const x6 = `<div class="productcard">
-    <img src="${cars[5].img}">
-    <div class="information">
-        <div class="name">${cars[5].name}</div>
-        <div class="description">${cars[5].year}</div>
-        <a class="ctabutton" href="#">${cars[5].price} kr</a>
-    </div>`;
-  c.innerHTML += x6;
-
-  const x7 = `<div class="productcard">
-    <img src="${cars[6].img}">
-    <div class="information">
-        <div class="name">${cars[6].name}</div>
-        <div class="description">${cars[6].year}</div>
-        <a class="ctabutton" href="#">${cars[6].price} kr</a>
-    </div>`;
-  c.innerHTML += x7;
-
-  const x8 = `<div class="productcard">
-    <img src="${cars[7].img}">
-    <div class="information">
-        <div class="name">${cars[7].name}</div>
-        <div class="description">${cars[7].year}</div>
-        <a class="ctabutton" href="#">${cars[7].price} kr</a>
-    </div>`;
-  c.innerHTML += x8;
-
-  const x9 = `<div class="productcard">
-    <img src="${cars[8].img}">
-    <div class="information">
-        <div class="name">${cars[8].name}</div>
-        <div class="description">${cars[8].year}</div>
-        <a class="ctabutton" href="#">${cars[8].price} kr</a>
-    </div>`;
-  c.innerHTML += x9;
-
-  const x10 = `<div class="productcard">
-    <img src="${cars[9].img}">
-    <div class="information">
-        <div class="name">${cars[9].name}</div>
-        <div class="description">${cars[9].year}</div>
-        <a class="ctabutton" href="#">${cars[9].price} kr</a>
-    </div>`;
-  c.innerHTML += x10;
+    productGallery.innerHTML += productCard;
+  }
 }
 
 console.log(cars);
